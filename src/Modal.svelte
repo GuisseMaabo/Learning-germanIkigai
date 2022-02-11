c70d15e3687a25e1ffbe635e4b3edded03451929
GuisseMaabo <cireguisse2015@gmail.com>
2022-02-11 16:17:19 +0100

    adding extra style in global.css and adding a coonditionnal class style

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,14 +1,28 @@
 <script>
-let showModal = false; 
+let showModal = true; 
+let isOpen = true;
 </script>
 
 {#if showModal}
-<div class="backdrop">
+<div class="backdrop" class:open={isOpen}>
     <div class="modal-content">
         <p> Sign up for free </p>
     </div>
 </div>
 {/if}
 <style>
-
+.backdrop{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0,0,0,0.8);
+}
+.modal-content{
+    padding: 10px;
+    border-radius: 10px;
+    max-width:400px;
+    margin: 10% auto;
+    text-align: center;
+    background:white;
+}
 </style>
