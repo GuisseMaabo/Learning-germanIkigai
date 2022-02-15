4b6ef70c6668126734efa46168c51b6ef56c491c
GuisseMaabo <cireguisse2015@gmail.com>
2022-02-15 11:24:11 +0100

    adding header and footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<footer>
+    <div class="copyright">Copyright 2022 Ikigai German</div>
+</footer>
+
+<style>
+    footer{
+        padding:40px;
+        text-align:center;
+    }
+    .copyright{
+        color: #aaa;
+        font-size:14px;
+        border-top: 1px solid #ddd;
+    }
+</style>
