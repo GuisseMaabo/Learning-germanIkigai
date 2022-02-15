92a4a1b4308ce078dcb2953442cfb4036673bcb8
GuisseMaabo <cireguisse2015@gmail.com>
2022-02-15 15:17:47 +0100

    reusable tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,25 @@
 <script>
 import Header from './components/Header.svelte';
 import Footer from './components/Footer.svelte';
+import Tabs from './shared/Tabs.svelte';
 
+
+
+// Tabs 
+
+let items = ['Current Courses', 'Add New Courses']
+let activeItems = 'Current Courses';
+const tabChange = (e)=> activeItems = e.detail;
 </script>
 
 <Header/>
 <main>
- 
+ <Tabs {activeItems} {items} on:tabChange={tabChange}/>
+ {#if activeItems === 'Current Courses'}
+	<p> courses list goes here </p>
+ {:else if activeItems === 'Add New Courses'}
+	<p> new courses form goes here</p>
+ {/if}
 </main>
 <Footer/>
 
