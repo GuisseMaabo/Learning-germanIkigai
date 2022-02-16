94ef9ce6d26287b4c5d1a9592403bbacd62e11f5
GuisseMaabo <cireguisse2015@gmail.com>
2022-02-16 15:09:51 +0100

    adding a form in svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
 import Header from './components/Header.svelte';
 import Footer from './components/Footer.svelte';
+import CreateCourseForm from './components/CreateCourseForm.svelte';
+
 import Tabs from './shared/Tabs.svelte';
 
 
@@ -9,16 +11,18 @@ import Tabs from './shared/Tabs.svelte';
 
 let items = ['Current Courses', 'Add New Courses']
 let activeItems = 'Current Courses';
+
 const tabChange = (e)=> activeItems = e.detail;
+
 </script>
 
 <Header/>
-<main>
+<main>""
  <Tabs {activeItems} {items} on:tabChange={tabChange}/>
  {#if activeItems === 'Current Courses'}
 	<p> courses list goes here </p>
  {:else if activeItems === 'Add New Courses'}
-	<p> new courses form goes here</p>
+	<CreateCourseForm />
  {/if}
 </main>
 <Footer/>
---
+++ src/components/CreateCourseForm.svelte
@@ -0,0 +1,40 @@
+<script>
+    let fields = {course:'', listening:'', writing:''};
+    const submitHandler = () => {
+       // console.log(fields);
+    }
+</script>
+
+<form   on:submit|preventDefault={submitHandler}>
+    <div class="form-field">
+        <label for="course">Course Field: </label>
+        <input type="text" id="field" bind:value={fields.course}>
+    </div>
+    <div class="form-field">
+        <label for="listening">Listening: </label>
+        <input type="text" id="listening" bind:value={fields.listening}>
+    </div>
+    <div class="form-field">
+        <label for="writing"> Writing: </label>
+        <input type="text" id="writing"  bind:value={fields.writing}>
+    </div>
+    <button>    Add a Course    </button>
+</form>
+<style>
+    form{
+        width:400px;
+        margin: 0 auto ;
+        text-align:center;
+    }
+    .form-field{
+        margin: 18px auto ;
+    }
+    input{
+        width:100%;
+        border-radius: 6px;
+    }
+    label{
+        margin: 10px auto;
+        text-align: left;
+    }
+</style>
